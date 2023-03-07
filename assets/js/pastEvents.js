084fc7289ae64e5d5f63140e2628868009ea4121
Gahi13 <sosagabriela777@gmail.com>
2023-03-07 12:10:32 -0300

    task2

---
+++ assets/js/pastEvents.js
@@ -0,0 +1,37 @@
+import data from "./amazing.js";
+
+function fechas_pasadas(data){
+  let past=[]
+  let fechaActual= new Date(data.currentDate )
+  console.log(fechaActual)
+  for (let i=0; i<data.events.length; i++) {
+      let fecha= new Date(data.events[i].date)
+      console.log(fecha)
+      if (fechaActual>fecha){
+          past.push(data.events[i])
+      }
+  }
+  return past
+}
+console.log(fechas_pasadas(data))
+const divElementos= document.getElementById('cards')
+let fragmento= document.createDocumentFragment();
+for (let eventos of fechas_pasadas(data)) {
+    const card = document.createElement('div')
+    card.classList.add('card')
+    card.style.width = '18rem'
+    card.innerHTML = ` 
+    <div class="card zoom bg-warning-subtle">
+    <img src="${eventos.image}" class="card-img-top" >
+    <div class="card-body">
+      <h5 class="card-title">${eventos.name}</h5>
+      <p class="card-text bg-info-subtle">${eventos.description}</p>
+      <div class=" d-grid gap-2">
+        <a href="./details.html" class="btn btn-outline-success me-md-2">Go to Details</a>
+      </div>
+    </div>
+    </div>
+    `
+    fragmento.appendChild(card)
+}
+divElementos.appendChild(fragmento)
---
+++ assets/js/upcomingEvents.js
@@ -0,0 +1,38 @@
+import data from "./amazing.js";
+
+function comparar_fechas(data){
+  let upcoming=[]
+  let fechaActual= new Date(data.currentDate )
+  console.log(fechaActual)
+  for (let i=0; i<data.events.length; i++) {
+      let fecha= new Date(data.events[i].date)
+      console.log(fecha)
+      if (fechaActual<fecha){
+          upcoming.push(data.events[i])
+      }
+  }
+  return upcoming
+}
+console.log(comparar_fechas(data))
+const divElementos= document.getElementById('cards')
+let fragmento= document.createDocumentFragment();
+for (let events of comparar_fechas(data)) {
+    const card = document.createElement('div')
+    card.classList.add('card')
+    card.style.width = '18rem'
+    card.innerHTML = ` 
+    <div class="card zoom bg-info-subtle">
+    <img src="${events.image}" class="card-img-top" >
+    <div class="card-body">
+      <h5 class="card-title">${events.name}</h5>
+      <p class="card-text bg-danger-subtle">${events.description}</p>
+      <div class=" d-grid gap-2 d-md-flex justify-content-md-center">
+        <a href="./details.html" class="btn btn-outline-success me-md-2">Go to Details</a>
+      </div>
+    </div>
+    </div>
+    `
+    fragmento.appendChild(card)
+}
+divElementos.appendChild(fragmento)
+
